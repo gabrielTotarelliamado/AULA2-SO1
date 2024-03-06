c046117e0b7c52d58957460b92428417007bb490
gabrielTotarelliamado <70151557+gabrielTotarelliamado@users.noreply.github.com>
2024-03-06 19:08:58 -0300

    Add files via upload

---
+++ principal2.java
@@ -0,0 +1,12 @@
+package view;
+
+import controller.KillController;
+
+public class principal2 {
+
+	public static void main(String[] args) {
+		KillController KC = new KillController();
+		KC.chamador();
+	}
+
+}
---
+++ principal3.java
@@ -0,0 +1,12 @@
+package view;
+
+import controller.DistroController;
+
+public class principal3 {
+
+	public static void main(String[] args) {
+	DistroController dc = new DistroController();
+	dc.chamador();
+	}
+
+}
